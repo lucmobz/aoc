a973deda0e15d9d414edf79f57413ccb569b4734
lucmobz <luca.mobz.work@gmail.com>
2022-10-05 17:56:47 +0000

    fix day 23

--- 2021/23/build.sh
+++ 2021/23/build.sh
@@ -3,10 +3,8 @@
 #CFLAGS="-std=c2x -g -march=native -Wall -Wextra -Wpedantic"
 CFLAGS="-std=c2x -Ofast -march=native"
 
-#cc $CFLAGS main1.c -o main1
-cc $CFLAGS main2.c -o main2
+cc $CFLAGS -Wno-unused-result main1.c -o main1
+cc $CFLAGS -Wno-unused-result main2.c -o main2
 
-#./main1 < sample.txt
-#./main1 < input.txt
-./main2 < sample.txt
-#./main2 < input.txt
+./main1 < input.txt
+./main2 < input.txt
