e71121e2a51e8b2bea36636e21c9f0c7a0ef09a3
lucmobz <luca.mobz.work@gmail.com>
2023-10-28 11:40:48 +0200

    added check day 1

--- 2022/01/build.sh
+++ 2022/01/build.sh
@@ -5,3 +5,8 @@ cc -Wall -Wextra -pedantic -g main2.c -o main2
 
 ./main1 < input.txt
 ./main2 < input.txt
+
+printf "\nCheck\nPart 1:\n"
+echo `paste -s -d+ input.txt | sed 's/++/\n/g' | bc | sort -g | tail -1`
+echo "Part 2:" 
+echo `paste -s -d+ input.txt | sed 's/++/\n/g' | bc | sort -g | tail -3 | paste -s -d+ | bc`
